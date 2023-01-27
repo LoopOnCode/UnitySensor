5872fbf07652928fc519f0ada1e42d593ef55c07
LoopOnTech <info@loopon.tech>
2023-01-27 12:46:31 +0000

    fixes #3 Use Adafruit NeoPixel library for controlling the RGB LED.

--- Arduino Examples/Rainbow/src/main.cpp
+++ Arduino Examples/Rainbow/src/main.cpp
@@ -1,13 +1,12 @@
 #include <Arduino.h>
-#include <FastLED.h>
+#include <Adafruit_NeoPixel.h>
 
 #define LED_PIN 2
 #define NUM_LEDS 1
-#define LED_CHIPSET WS2812
 #define BRIGHTNESS 255
-#define DELAY_MS 25
+#define DELAY_MS 30
 
-CRGB leds[NUM_LEDS];
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
@@ -15,19 +14,21 @@ void setup()
   Serial.setTxTimeoutMs(0);
   Serial.println("Starting...");
 
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show();
+  strip.setBrightness(BRIGHTNESS);
 }
 
 void loop()
 {
-  for (int j = 0; j < 255; j++)
+  for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256)
   {
-    for (int i = 0; i < NUM_LEDS; i++)
+    for (int i = 0; i < strip.numPixels(); i++)
     {
-      leds[i] = CHSV(i - (j * 2), 255, BRIGHTNESS);
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
     }
-    FastLED.show();
+    strip.show();
     delay(DELAY_MS);
   }
 }
